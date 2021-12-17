86ec8bda6fc48c06cf75eed2f3607bc6fc796e9b
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-17 11:05:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+
+ document.getElementById('btn').addEventListener('click', function(){
+  var char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+_-&@*()0123456789!$^%#/?';
+ var length = 12;
+  var password = '';
+  for(let i = 0; i < length; i++ ){
+
+
+
+
+
