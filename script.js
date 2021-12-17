999ca9ee6d9a8ceef937bfc46e01f4349696d850
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-17 11:06:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,17 @@
  var length = 12;
   var password = '';
   for(let i = 0; i < length; i++ ){
+  
+   
+       password +=char.charAt(Math.floor(Math.random()*char.length));
+  //  var randomNBR= Math.floor(Math.random()*char.length);
+  //   password += char.substring(randomNBR , randomNBR + 1 );
+  }
+ document.getElementById('password').value=password;
+ })
 
 
-
+   
+   
 
 
